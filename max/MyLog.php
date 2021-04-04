8d119b72638a5fe71f8a48dff9125563d8902bda
NilAdm1rari <NilAdm1rari>
2021-04-04 23:22:56 +0300

    lab9

--- max/MyLog.php
+++ max/MyLog.php
@@ -6,6 +6,8 @@ use core\LogInterface;
 
 Class MyLog extends LogAbstract implements LogInterface {
 
+
+
     public function _log(String $str){
         $this->log[]=$str;
     }
@@ -13,14 +15,19 @@ Class MyLog extends LogAbstract implements LogInterface {
     /**
      * @param String $str строка для записи в массив лога
      */
-    public static function log(String $str){
+    public static function log(string $str){
         self::Instance()->_log($str);
 	}
     
     public function _write(){
-        
+
+
+        $d = date_create();
+        $z = (string)$d->format('d-m-Y\TH.i.s.u');
         foreach($this->log as $value){
             echo $value."\n";
+
+            file_put_contents(__DIR__ . "\..\log\\".$z.".log", trim($value."\r\n") . PHP_EOL, FILE_APPEND);
         }
         
     }
