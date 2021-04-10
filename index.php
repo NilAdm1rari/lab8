ab0b8d9646065afc6942e8ce9f31e26cc128ad3c
Your Name <you@example.com>
2021-04-10 06:15:14 +0300

    lab10

--- index.php
+++ index.php
@@ -3,19 +3,23 @@
 use max\MyLog;
 use max\QuEquation;
 
-include "core/core/EquationInterface.php";
+/*include "core/core/EquationInterface.php";
 include "core/core/LogAbstract.php";
 include "core/core/LogInterface.php";
 include "max/MyLog.php";
 include "max/Equation.php";
 include "max/QuEquation.php";
-include "max/Exception.php";
+include "max/Exception.php";*/
+require_once __DIR__ . './vendor/autoload.php';
 
 ini_set("display_errors", 1);
 error_reporting(-1);
 
 try {
-    MyLog::log("Версия программы: " . trim(file_get_contents('version')) );
+	if(!is_dir("log")) {
+        mkdir("log", 0700);
+    }
+    //MyLog::log("Версия программы: " . trim(file_get_contents('version')) );
     $b = new QuEquation();
     $values = array();
 
